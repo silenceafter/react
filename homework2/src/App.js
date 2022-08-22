7d9fb16c556ecedc4706c7958fea69684b830061
prog-5 <a59-info@yandex.ru>
2022-08-22 16:34:20 +0300

    Задание1-5 Обновление 22.08.22

--- homework2/src/App.js
+++ homework2/src/App.js
@@ -1,25 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-export function DDD() {
-  const [messageList, setMessageList] = useState([]);
-  const updateMessageList = () => {
-    setMessageList({ text: "", author: ""});
-  };
-
-
+export function DDD(props) {
   /*useEffect(() => {
     console.log(messageList);
-  }, [messageList]);*/
-  return (<div>{messageList}</div>);//messageList.map((messageList) => <div>{messageList}</div>);   
+  }, [messageList]);
+  return (
+    <div>
+      {messageList.map((element, index) => {
+        return (
+          <div key={index}>
+            <h2>{element.id} текст: {element.text}, сообщение: {element.author}</h2>
+          </div>
+        );
+      })}
+    </div>
+  );*/
 }
 
 function App() {
+  const [messageList, setMessageList] = useState([]);
+  const updateMessageList = (props) => {
+    setMessageList(current => [...current, {id: 1, text: props.message, author: 'kkk'}]);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        {DDD()}
-      </header>
+        <form onSubmit={updateMessageList}>
+          <fieldset>
+            <label>
+              <p>Сообщение: </p>
+              <input name='message'/>
+            </label>
+          </fieldset>
+          <button type='submit'>Отправить</button>
+        </form>
+        <div>
+          {messageList.map((element, index) => {
+            return (
+              <div key={index}>
+                <h2>{element.id} текст: {element.text}, сообщение: {element.author}</h2>
+              </div>
+            );
+          })}
+        </div> 
+      </header>            
     </div>
   );
 }
