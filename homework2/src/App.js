7768f4dac79729ecd7be82b35c44e17b63615c51
prog-5 <a59-info@yandex.ru>
2022-08-24 16:33:23 +0300

    Задание1-5 Обновление 24.08.22 #2

--- homework2/src/App.js
+++ homework2/src/App.js
@@ -36,25 +36,64 @@ export function DDD(props) {
 
 function App() {
   const [messageList, setMessageList] = useState([]);
-  const updateMessageList = (event) => {
-    const formData = new FormData(event.currentTarget);
+  const updateMessageList = (props) => {
+    const formData = new FormData(props.currentTarget);
+    let author = '',
+      message = '';
+    //
     for(let [key, value] of formData.entries()) {
-      //if (value.name.trim().toLowerCase() == 'message') {
-        console.log(key, value);
-        setMessageList(current => [...current, {id: 1, text: value, author: 'kkk'}]);//event.target.value
-        //break;
-      //}          
+      //message
+      if (key.trim().toLowerCase() == 'message')
+        message = value;
+
+      //author
+      if (key.trim().toLowerCase() == 'author')
+        author = value;
     }
-    //setMessageList(current => [...current, {id: 1, text: 'ddd', author: 'kkk'}]);
-    event.preventDefault();
+    setMessageList(current => [...current, {text: message, author: author}]);
+    //event.preventDefault();
   };
+  /*const updateMessageList = (event) => {
+    const formData = new FormData(event.currentTarget);
+    let author = '',
+      message = '';
+    //
+    for(let [key, value] of formData.entries()) {
+      //message
+      if (key.trim().toLowerCase() === 'message')
+        message = value;
+
+      //author
+      if (key.trim().toLowerCase() === 'author')
+        author = value;
+    }
+    setMessageList(current => [...current, {text: message, author: author}]);
+    event.preventDefault();
+  };*/
+
+  const ggg = () => {
+    //setMessageList(current => [...current, {text: 'message', author: 'author'}]);
+  };
+  
+  useEffect(() => {
+    let gg = messageList.text;
+    let g = 5;
+    ggg();
+    //const lastMessage = messageList[messageList.length - 1];
+    /*if (lastMessage != null) {
+      return () => {
+        setMessageList(current => [...current, {text: 'message', author: 'author'}]);
+      };
+    }*///setMessageList({text: '11', author: '22'});
+  }, [messageList.text]);
+
   return (
     <div className="App">
       <header className="App-header">
         <form onSubmit={updateMessageList}>
           <fieldset>
-            Автор: <input type="text" onChange={event => updateMessageList} name='author'/><br/>
-            Сообщение: <input type="text" onChange={event => updateMessageList} name='message'/>
+            <input type="text" name='author' placeholder='Автор:' onChange={event => updateMessageList}/><br/>
+            <input type="text" name='message' placeholder='Сообщение:' onChange={event => updateMessageList}/>
           </fieldset>
           <button type='submit'>Отправить</button>
         </form>
