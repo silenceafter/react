ba03f50f96cf2f9daef46b53a9d60d4df6f7e7cc
silenceafter <a59-info@yandex.ru>
2022-08-24 06:32:10 +0300

    Задание1-5 Обновление 24.08.22

--- homework2/src/App.js
+++ homework2/src/App.js
@@ -37,16 +37,15 @@ export function DDD(props) {
 function App() {
   const [messageList, setMessageList] = useState([]);
   const updateMessageList = (event) => {
-    for(let value of event.target) {
-      if (value.name.trim().toLowerCase() == 'message') {
-        console.log('find');  
-        break;
-      }          
+    const formData = new FormData(event.currentTarget);
+    for(let [key, value] of formData.entries()) {
+      //if (value.name.trim().toLowerCase() == 'message') {
+        console.log(key, value);
+        setMessageList(current => [...current, {id: 1, text: value, author: 'kkk'}]);//event.target.value
+        //break;
+      //}          
     }
-    //
-    console.log(event.target[1].value);
     //setMessageList(current => [...current, {id: 1, text: 'ddd', author: 'kkk'}]);
-    setMessageList(current => [...current, {id: 1, text: 'ddd', author: 'kkk'}]);//event.target.value
     event.preventDefault();
   };
   return (
@@ -54,13 +53,20 @@ function App() {
       <header className="App-header">
         <form onSubmit={updateMessageList}>
           <fieldset>
-            <label>
-              <p>Сообщение: </p>
-              <input type="text" onChange={event => updateMessageList} name='message'/>
-            </label>
+            Автор: <input type="text" onChange={event => updateMessageList} name='author'/><br/>
+            Сообщение: <input type="text" onChange={event => updateMessageList} name='message'/>
           </fieldset>
           <button type='submit'>Отправить</button>
         </form>
+        {<div>
+          {messageList.map((element, index) => {
+            return (
+              <div key={index}>
+                <h2>{element.id} текст: {element.text}, сообщение: {element.author}</h2>
+              </div>
+            );
+          })}
+        </div>}
       </header>
     </div>
   );
